688775b7cf35d6ad32f7622efb32ffa845c44972
Dumindu Kavishka <w.k.dumindukavishka@outlook.com>
2023-09-04 23:48:23 +0530

    token generate and writing to the database

--- src/main/java/com/example/commercialsite/entity/Item.java
+++ src/main/java/com/example/commercialsite/entity/Item.java
@@ -2,6 +2,7 @@ package com.example.commercialsite.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -10,7 +11,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-
+@Getter
 @Entity
 @Table(name = "Item")
 public class Item {
@@ -29,17 +30,17 @@ public class Item {
     @Column(name = "gender",nullable = false)
     private String gender;
 
-    @Column(name = "quality_status")
-    private String qualityStatus;
+//    @Column(name = "quality_status")
+//    private String qualityStatus;
 
     @Column(name = "type",nullable = false)
     private String type;
 
-    @Column(name = "price_range")
-    private String priceRange;
+    @Column(name = "price")
+    private int price;
 
-    @Column(name = "active_status")
-    private boolean activeState;
+    @Column(name = "available_status")
+    private boolean availableStatus;
 
     @Column(name = "size", nullable = false)
     private String size;
@@ -48,15 +49,4 @@ public class Item {
     @JoinColumn(name = "RequestTokenId", referencedColumnName = "requestTokenId")
     private RequestToken requestToken;
 
-
-    public Item(String color, String imageURL, String gender, String type, String priceRange, boolean activeState, String size, RequestToken requestToken) {
-        this.color = color;
-        this.imageURL = imageURL;
-        this.gender = gender;
-        this.type = type;
-        this.priceRange = priceRange;
-        this.activeState = activeState;
-        this.size = size;
-        this.requestToken = requestToken;
-    }
 }
--- src/main/java/com/example/commercialsite/entity/RequestToken.java
+++ src/main/java/com/example/commercialsite/entity/RequestToken.java
@@ -2,15 +2,15 @@ package com.example.commercialsite.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.NotFound;
-import org.hibernate.annotations.NotFoundAction;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "RequestTokens")
+@Getter
+@Entity(name = "RequestToken")
+@Table(name = "request_token")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -18,15 +18,15 @@ public class RequestToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long requestTokenId;
-
     private Long customerId;
     private Long qualityCheckerId;
-
     private int status;
     private String itemDescription;
     private String itemImage;
     private LocalDateTime requestDateTime;
 
+
+    // data jpa relationship
     @OneToOne(mappedBy = "requestToken", cascade = CascadeType.ALL)
     private Item item;
 
