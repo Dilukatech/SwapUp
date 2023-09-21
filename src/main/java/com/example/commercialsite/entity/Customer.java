ac221135248eefe6ea9b3a0411e38ddba6b3fd27
Dilukatech <dilukaofficial97@gmail.com>
2023-09-21 13:58:17 +0530

    update master

--- src/main/java/com/example/commercialsite/entity/Customer.java
+++ src/main/java/com/example/commercialsite/entity/Customer.java
@@ -6,12 +6,12 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity
+@Entity(name = "Customer")
 @Table(name = "customer")
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class Customer {
+public class Customer extends Users{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "token_id")
--- src/main/java/com/example/commercialsite/entity/Users.java
+++ src/main/java/com/example/commercialsite/entity/Users.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 @AllArgsConstructor //
 @NoArgsConstructor
 @Getter
-@Entity(name = "User") // hibernate annotations
+@Entity(name = "Users") // hibernate annotations
 @Table(name = "users")
 @Data
 public class Users {
