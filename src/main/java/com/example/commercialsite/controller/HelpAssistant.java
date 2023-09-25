0cb413993127cd090f9be4b1d26e0a4d37dde8df
DilukaDeshan <dilukaofficial97@gmail.com>
2023-09-25 14:26:02 +0530

    change post to put in HelpAssistant Controller

--- src/main/java/com/example/commercialsite/controller/HelpAssistant.java
+++ src/main/java/com/example/commercialsite/controller/HelpAssistant.java
@@ -6,10 +6,7 @@ import com.example.commercialsite.utill.StandardResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/help-assistant")
@@ -17,7 +14,7 @@ public class HelpAssistant {
   @Autowired
   private HelpAssistantService helpAssistantService;
 
-    @PostMapping("/CheckHelpRequestFromHelpAssistant")
+    @PutMapping("/CheckHelpRequestFromHelpAssistant")
     public ResponseEntity<StandardResponse> CheckHelpRequestFromHelpAssistant(@RequestBody CheckHelpDto checkHelpDto){
         return helpAssistantService.CheckHelpRequest(checkHelpDto);
     }
