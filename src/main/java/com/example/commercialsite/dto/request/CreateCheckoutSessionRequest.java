97392d463605f2eb086578717b39492ddb0f5ca9
Sanjeewa Yapa <aquakloud0829@gmail.com>
2023-10-31 12:01:22 +0530

    Payment Gateway (checkoutController)

---
+++ src/main/java/com/example/commercialsite/dto/request/CreateCheckoutSessionRequest.java
@@ -0,0 +1,15 @@
+package com.example.commercialsite.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CreateCheckoutSessionRequest {
+    private String priceId;
+    private String planName;
+    private Long userId;
+    private double price;
+}
